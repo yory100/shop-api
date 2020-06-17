c57f82946cadc867c718ba7ba358f6180c1de8f8
yyord <yyord@softserveinc.com>
2020-06-17 20:20:13 +0300

    add delete in touter

--- products/productsRoutes.js
+++ products/productsRoutes.js
@@ -5,7 +5,7 @@ module.exports = (Controller) => {
   router.get('/', Controller.getList);
   router.post('/', Controller.create);
   router.put('/:id', Controller.update);
-  // router.delete('/:id', Controller.delete);
+  router.delete('/:id', Controller.remove);
 
     return router;
 };
